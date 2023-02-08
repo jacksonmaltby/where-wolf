ecf5d058ec4bb43973b0fb486343b9f68712af3b
Kyle Parks <107071647+Goobber33@users.noreply.github.com>
2023-02-07 18:47:01 -0800

    Changed const to seattle from uluru

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,15 @@
 
   function initMap() {
     // The location of Uluru
-    const uluru = { lat: 47.608013, lng: -122.335167 };
+    const seattle = { lat: 47.608013, lng: -122.335167 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 11,
-      center: uluru,
+      center: seattle,
     });
     // The marker, positioned at Uluru
     const marker = new google.maps.Marker({
-      position: uluru,
+      position: seattle,
       map: map,
     });
   }
