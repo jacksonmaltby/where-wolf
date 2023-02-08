221ffdc1c76dc09b5b66c4a4e42360971ae0d7bb
Kyle Parks <107071647+Goobber33@users.noreply.github.com>
2023-02-07 18:50:11 -0800

    Changed Uluru to Seattle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 // Initialize and add the map
 
   function initMap() {
-    // The location of Uluru
+    // The location of Seattle
     const seattle = { lat: 47.608013, lng: -122.335167 };
-    // The map, centered at Uluru
+    // The map, centered at Seattle
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 11,
       center: seattle,
     });
-    // The marker, positioned at Uluru
+    // The marker, positioned at Seattle
     const marker = new google.maps.Marker({
       position: seattle,
       map: map,
