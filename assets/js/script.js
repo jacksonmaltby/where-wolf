0f83f4633478ab471ad4f22fc8fcd6cf93013750
Kyle Parks <107071647+Goobber33@users.noreply.github.com>
2023-02-08 20:56:29 -0800

    Added function to listen for which selection from the drop down is chosen, and the checkbox. Once the user types in their location, the listed businesses will drop down to a list, and show on the map where they are located.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
 
+// Global variables
+
 const API_KEY = "T-SjguE8Kty3cVRNzp2IbdePc7PMKbZ9RYOF_U-m8iZWq2cdY_wKniY5mlZ7lbwUdnrFzQebI5zWkIhiJfpQiZuyIXnuxhXyJnZZensCwfWl7rehqqLyRxugkBzjY3Yx";
 const searchTerm = document.querySelector("#searchTerm");
 const searchButton = document.querySelector("#searchButton");
 const results = document.querySelector("#yelp-results");
-const map = L.map("map").setView([51.505, -0.09], 13);
+const map = L.map("map").setView([47.6062, -122.3321], 11);
+const category = document.querySelector("#business-types");
+const dogFriendlyCheckbox = document.querySelector("#dogs-allowed");
+
+// The map that displays on the right side of the page
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
@@ -11,19 +17,35 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 searchButton.addEventListener("click", async () => {
   const location = searchTerm.value;
-  const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=dog+friendly&location=${location}`, {
+  let term = "dog+friendly+";
+  if (!dogFriendlyCheckbox.checked) {
+    term = "";
+  }
+  term += category.value;
+  
+  const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}`, {
     headers: {
       Authorization: `Bearer ${API_KEY}`
     }
   });
+
   const data = await response.json();
 
   results.innerHTML = "";
   data.businesses.forEach(business => {
     const li = document.createElement("li");
-    li.innerHTML = `<h3>${business.name}</h3><p>${business.location.address}</p>`;
+    li.innerHTML = `<h3>${business.name}</h3><p>${business.location.address}</p><p>${business.location.city}</p><p>${business.location.state}</p><p>${business.location.zip_code}</p><p>${category.value}</p>`;
     results.appendChild(li);
+
+    let bounds = L.latLngBounds();
+  data.businesses.forEach(business => {
+    bounds.extend([business.coordinates.latitude, business.coordinates.longitude]);
+  });
+  map.fitBounds(bounds);
+
+    // Creates a marker for each business
+    
     const marker = L.marker([business.coordinates.latitude, business.coordinates.longitude]).addTo(map);
-    marker.bindPopup(`<h3>${business.name}</h3><p>${business.location.address}</p>`);
+    marker.bindPopup(`<h3>${business.name}</h3><p>${business.location.address}</p><p>${category.value}</p>`);
   });
 });
