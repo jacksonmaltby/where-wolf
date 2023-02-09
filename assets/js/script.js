88cf555cfe3401100b99bb1cc4a85bbd850de2d1
Kyle Parks <107071647+Goobber33@users.noreply.github.com>
2023-02-09 14:57:58 -0800

    Added a next button and previous button. Made them functional by adding the same code from script.js and adding a + or - to the search. Everything is functional, and will show the next businesses and previous. I still need to change the parameters, but everything works this far. I also set the search to only look up 4 businesses at a time.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-
-// Global variables
-
 const API_KEY = "T-SjguE8Kty3cVRNzp2IbdePc7PMKbZ9RYOF_U-m8iZWq2cdY_wKniY5mlZ7lbwUdnrFzQebI5zWkIhiJfpQiZuyIXnuxhXyJnZZensCwfWl7rehqqLyRxugkBzjY3Yx";
 const searchTerm = document.querySelector("#searchTerm");
 const searchButton = document.querySelector("#searchButton");
@@ -12,40 +9,51 @@ const dogFriendlyCheckbox = document.querySelector("#dogs-allowed");
 // The map that displays on the right side of the page
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+attribution: "© <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
 }).addTo(map);
 
+let offset = 0;
+
 searchButton.addEventListener("click", async () => {
-  const location = searchTerm.value;
-  let term = "dog+friendly+";
-  if (!dogFriendlyCheckbox.checked) {
-    term = "";
-  }
-  term += category.value;
-  
-  const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}`, {
-    headers: {
-      Authorization: `Bearer ${API_KEY}`
-    }
-  });
+const location = searchTerm.value;
+let term = "dog+friendly+";
+if (!dogFriendlyCheckbox.checked) {
+term = "";
+}
+term += category.value;
 
-  const data = await response.json();
-
-  results.innerHTML = "";
-  data.businesses.forEach(business => {
-    const li = document.createElement("li");
-    li.innerHTML = `<h3>${business.name}</h3><p>${business.location.address}</p><p>${business.location.city}</p><p>${business.location.state}</p><p>${business.location.zip_code}</p><p>${category.value}</p>`;
-    results.appendChild(li);
-
-    let bounds = L.latLngBounds();
-  data.businesses.forEach(business => {
-    bounds.extend([business.coordinates.latitude, business.coordinates.longitude]);
-  });
-  map.fitBounds(bounds);
-
-    // Creates a marker for each business
-    
-    const marker = L.marker([business.coordinates.latitude, business.coordinates.longitude]).addTo(map);
-    marker.bindPopup(`<h3>${business.name}</h3><p>${business.location.address}</p><p>${category.value}</p>`);
-  });
+const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&offset=${offset}&limit=4`, {
+headers: {
+Authorization: `Bearer ${API_KEY}`
+}
 });
+
+const data = await response.json();
+
+results.innerHTML = "";
+data.businesses.forEach(business => {
+const li = document.createElement("li");
+let description = "N/A";
+if (business.description) {
+description = business.description;
+}
+li.innerHTML = `<h3>${business.name}</h3><p>${business.location.address}</p><p>${business.location.city}</p><p>${business.location.state}</p><p>${business.location.zip_code}</p><p>${category.value}</p><p>${description}</p>`;
+results.appendChild(li);
+});
+
+let bounds = L.latLngBounds();
+data.businesses.forEach(business => {
+  bounds.extend([business.coordinates.latitude, business.coordinates.longitude]);
+});
+map.fitBounds(bounds);
+
+// Creates a marker for each business
+
+data.businesses.forEach(business => {
+const marker = L.marker([business.coordinates.latitude, business.coordinates.longitude]).addTo(map);
+marker.bindPopup(`<h3>${business.name}</h3>`);
+});
+});
+
+
+
