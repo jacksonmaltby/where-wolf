7c974765cb337f47d1c8affc42feb30c16ed07bd
Brandon Guerrero <brandonguerrero@Brandons-MacBook-Air.local>
2023-02-13 18:53:49 -0800

    fixed undefined bug, added business descriptions, and hid map

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,9 +14,12 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 let offset = 0;
-
+document.querySelector("#map").style.display = "none";
+// Search button that calls the Yelp Fusion API and returns businesses that are dog friendly.
+// Added dogFriendlyCheckbox for a user to select if they want to add dog friendly businesses or not
 searchButton.addEventListener("click", async () => {
-  const location = searchTerm.value;
+  document.querySelector("#map").style.display = "block";
+  var location = searchTerm.value;
   saveLocation(location);
   let term = "dog+friendly+";
   if (!dogFriendlyCheckbox.checked) {
@@ -41,6 +44,8 @@ console.log(data)
     if (business.description) {
       description = business.description;
     }
+    // Console log line 38 to retrive the business data and implemented into line 48. fixed business.location.address into business.location.address1
+    business.location
     div.innerHTML = `<h3 class="">${business.name}</h3><p class="">${business.location.address1}</p><p class="">${business.location.city}</p><p class="">${business.location.state}</p><p class="">${business.location.zip_code}</p><p class="">${category.value}</p><p class="">${business.price}</p <p class="">${business.rating}</p> <p class="">${business.phone}</p <a class="">${business.url}</a`;
     results.appendChild(div);
   });
