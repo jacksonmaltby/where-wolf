70524ac5cd6ee1d4f7556e0b7dab9d152011fc0e
Brandon Guerrero <brandonguerrero@Brandons-MacBook-Air.local>
2023-02-13 17:29:29 -0800

    fixed undefined bug in search results, as well as added search descriptions

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ const dogFriendlyCheckbox = document.querySelector("#dogs-allowed");
 
 // The map that displays on the right side of the page
 
+const url = "https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&offset=${offset}&limit=4`"
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "© <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
 }).addTo(map);
@@ -22,15 +23,16 @@ searchButton.addEventListener("click", async () => {
     term = "";
   }
   term += category.value;
-
   const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&offset=${offset}&limit=4`, {
     headers: {
       Authorization: `Bearer ${API_KEY}`
+  
     }
+  
   });
 
   const data = await response.json();
-
+console.log(data)
   results.innerHTML = "";
   data.businesses.forEach(business => {
     const div = document.createElement("div");
@@ -39,7 +41,7 @@ searchButton.addEventListener("click", async () => {
     if (business.description) {
       description = business.description;
     }
-    div.innerHTML = `<h3 class="">${business.name}</h3><p class="">${business.location.address}</p><p class="">${business.location.city}</p><p class="">${business.location.state}</p><p class="">${business.location.zip_code}</p><p class="">${category.value}</p><p class="">${description}</p>`;
+    div.innerHTML = `<h3 class="">${business.name}</h3><p class="">${business.location.address1}</p><p class="">${business.location.city}</p><p class="">${business.location.state}</p><p class="">${business.location.zip_code}</p><p class="">${category.value}</p><p class="">${business.price}</p <p class="">${business.rating}</p> <p class="">${business.phone}</p <a class="">${business.url}</a`;
     results.appendChild(div);
   });
 
